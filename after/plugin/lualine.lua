7136c5b180ee5a6bc8b0d1ee17b26e2637065bbf
Sergey <sergfox321@gmail.com>
2024-06-15 11:51:24 +0300

    Statusline was configured

---
+++ after/plugin/lualine.lua
@@ -0,0 +1,24 @@
+require('lualine').setup({
+    options = {
+        icons_enabled = false,
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = {
+            {
+                'filename',
+                path = 1,
+                symbols = {
+                    -- modified = '[+]',
+                    readonly = '[Read]',
+                },
+            }
+        },
+        lualine_x = { 'diagnostics' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+    },
+})
