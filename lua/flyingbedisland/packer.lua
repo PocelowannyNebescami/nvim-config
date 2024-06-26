65efb9fe317920a5f5646c75fc5c73d6d531ad13
Sergey <sergfox321@gmail.com>
2024-06-26 12:11:17 +0300

    Packer config was refactored

--- lua/flyingbedisland/packer.lua
+++ lua/flyingbedisland/packer.lua
@@ -1,15 +1,9 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
-    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
     use {
         'nvim-telescope/telescope.nvim', branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     use {
@@ -21,28 +15,27 @@ return require('packer').startup(function(use)
         }
     }
 
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use { 'nvim-treesitter/nvim-treesitter',  run = ':TSUpdate' }
 
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
         requires = {
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-
-            {'neovim/nvim-lspconfig'},
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'L3MON4D3/LuaSnip'},
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+            { 'neovim/nvim-lspconfig' },
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
         }
 
     }
 
-    use { "lewis6991/gitsigns.nvim" }
+    use "lewis6991/gitsigns.nvim"
 
-    use { "HoNamDuong/hybrid.nvim" }
+    use "HoNamDuong/hybrid.nvim"
 
-    use { 'nvim-lualine/lualine.nvim' }
+    use 'nvim-lualine/lualine.nvim'
 
     use { 'mcauley-penney/visual-whitespace.nvim', config = true }
 
