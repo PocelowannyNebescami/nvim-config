dd2ca0725316686065f6a220f7af399973c1f84b
Sergey <sergfox321@gmail.com>
2024-08-25 23:30:54 +0300

    Borders were added to lsp windows

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -100,3 +100,13 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
     end
 })
+
+local border_style = "rounded"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+    vim.lsp.handlers.hover, { border = border_style }
+)
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+    vim.lsp.handlers.signature_help, { border = border_style }
+)
+vim.diagnostic.config{ float = { border = border_style } }
