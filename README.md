769f196f02550e11687551c9ae79c1393b292058
Sergey <sergfox321@gmail.com>
2024-11-23 01:11:06 +0300

    Packer install was automated

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-# nvim config
-
-## Install
-
-Clone Packer
-
-```sh
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-```
-
-Syncrhonize Packer
-
-```vim
-:PackerSync
-```
+# Neovim config
--- lua/packer_config.lua
+++ lua/packer_config.lua
@@ -1,3 +1,22 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local packer_path = 'packer/start/packer.nvim'
+
+    local install_path = fn.stdpath('data')..'/site/pack/'..packer_path
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({
+            'git', 'clone', '--depth', '1',
+            'https://github.com/wbthomason/packer.nvim',
+            install_path
+        })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
@@ -47,4 +66,7 @@ return require('packer').startup(function(use)
 
     use 'stevearc/oil.nvim'
 
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
