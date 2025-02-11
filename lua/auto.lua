ca3fd4ca80a6b23adae0eacacb0f56e2d893856b
Sergey <sergfox321@gmail.com>
2025-02-11 21:11:58 +0300

    Automatic formatting was added for go files

--- lua/auto.lua
+++ lua/auto.lua
@@ -9,3 +9,25 @@ autocmd('BufWritePre', {
         vim.fn.winrestview(view)
     end
 })
+
+local gofumpt_exists = vim.fn.executable("gofumpt") == 1
+local golines_exists = vim.fn.executable("golines") == 1
+local formatter
+
+if gofumpt_exists then
+    formatter = "gofumpt"
+elseif golines_exists then
+    formatter = "golines"
+else
+    formatter = "go fmt"
+end
+
+local function format_go()
+    vim.cmd("silent !" .. formatter .. " -w %")
+    vim.cmd("checktime")
+end
+
+autocmd('BufWritePost', {
+    pattern = "*.go",
+    callback = format_go,
+})
