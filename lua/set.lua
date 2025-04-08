849b2251f00896b653ec0995533d1fb8416f340c
Sergey <sergfox321@gmail.com>
2025-04-08 23:48:48 +0300

    Clipboard for WSL was added

--- lua/set.lua
+++ lua/set.lua
@@ -40,3 +40,18 @@ vim.diagnostic.config({
         prefix = '🤓',
     }
 })
+
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
