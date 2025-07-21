01271cd2e4a216288712015b7bc352d1c24dd5a7
Sergey <sergfox321@gmail.com>
2025-07-21 23:08:40 +0300

    Completion was configured with blink.cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,21 +3,31 @@ return {
         'mason-org/mason-lspconfig.nvim',
         version = 'v1.*',
         dependencies = {
-            { 'mason-org/mason.nvim', version = 'v1.*', opts = {} },
+            { 'mason-org/mason.nvim',  version = 'v1.*', opts = {} },
             { 'neovim/nvim-lspconfig', version = 'v1.*' },
+            { 'saghen/blink.cmp', version = '1.*' },
         },
         opts = {
             ensure_installed = { 'lua_ls', 'marksman' },
             handlers = {
                 -- default handler
                 function(server_name)
-                    require("lspconfig")[server_name].setup()
+                    local capabilities =
+                        require('blink.cmp').get_lsp_capabilities()
+
+                    require("lspconfig")[server_name].setup({
+                        capabilities = capabilities,
+                    })
                 end,
-                ["lua_ls"] = function ()
+                ["lua_ls"] = function()
+                    local capabilities =
+                        require('blink.cmp').get_lsp_capabilities()
+
                     local runtime_path = vim.split(package.path, ';')
                     table.insert(runtime_path, "lua/?.lua")
                     table.insert(runtime_path, "lua/?/init.lua")
                     require('lspconfig').lua_ls.setup({
+                        capabilities = capabilities,
                         settings = {
                             Lua = {
                                 telemetry = { enable = false },
@@ -37,8 +47,12 @@ return {
                         },
                     })
                 end,
-                ["gopls"] = function ()
+                ["gopls"] = function()
+                    local capabilities =
+                        require('blink.cmp').get_lsp_capabilities()
+
                     require('lspconfig').gopls.setup({
+                        capabilities = capabilities,
                         filetypes = {
                             'go',
                             'gomod',
