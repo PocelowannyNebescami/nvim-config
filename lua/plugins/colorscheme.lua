be10f48dfbbf99fa6b065a8acdfb8a9c1e63d141
Sergey <sergfox321@gmail.com>
2025-12-22 21:37:21 +0300

    feat: transparent background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,30 +1,10 @@
-local function replace(dict, from, to)
-    if type(dict) ~= "table" then
-        return dict
-    end
-
-    for k, v in pairs(dict) do
-        if type(v) == "table" then
-            dict[k] = replace(v, from, to)
-        elseif v == from then
-            dict[k] = to
-        end
-    end
-
-    return dict
-end
-
 return {
     "HoNamDuong/hybrid.nvim",
-    opts = {
-    },
+    lazy = false,
+    priority = 1000,
     config = function()
         require('hybrid').setup({
-            overrides = function(hl, c)
-                hl.ColorColumn = { bg = "#212327" }
-
-                hl = replace(hl, c.bg, c.bg_hard)
-            end,
+            transparent = true,
         })
 
         vim.cmd.colorscheme('hybrid')
